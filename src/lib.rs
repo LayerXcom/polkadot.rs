b8d342710af2a743808288d0f6e470d1b1c0b2c1
Osuke Sudo <dish230@gmail.com>
2019-05-30 12:17:38 +0900

    Implement Submit handler

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@ pub mod utils;
 use utils::*;
 
 const WS_URL_LOCAL: &str = "ws://127.0.0.1:9944";
+const REQUEST_SUBMIT: u32 = 3;
 
 pub enum Url {
     Local,
@@ -17,34 +18,20 @@ pub enum Url {
 }
 
 pub struct Api {
-    url: String,
-    genesis_hash: Hash,
+    url: String
 }
 
 impl Api {
     pub fn init(url: Url) -> Result<Self> {
-        let json_req = json!({
-            "method": "chain_getBlockHash",
-            "params": [0],
-            "jsonrpc": "2.0",
-            "id": "1",
-        });
-
         match url {
             Url::Local => {
-                let genesis_hash_str = get_response(WS_URL_LOCAL, json_req.to_string())?;
-
                 Ok(Api {
-                    url: WS_URL_LOCAL.to_owned(),
-                    genesis_hash: hexstr_to_hash(genesis_hash_str)
+                    url: WS_URL_LOCAL.to_owned()
                 })
             },
             Url::Custom(url) => {
-                let genesis_hash_str = get_response(url, json_req.to_string())?;
-
                 Ok(Api {
-                    url: url.to_owned(),
-                    genesis_hash: hexstr_to_hash(genesis_hash_str)
+                    url: url.to_owned()
                 })
             }
         }
@@ -60,6 +47,140 @@ impl Api {
         });
 
         get_response(&self.url[..], req.to_string())
+    }
+
+    pub fn submit_extrinsic(&self, params: String) -> Result<Hash> {
+        let req = json!({
+            "method": "author_submitAndWatchExtrinsic",
+            "params": [params],
+            "jsonrpc": "2.0",
+            "id": REQUEST_SUBMIT.to_string(), // TODO
+        });
+
+        submit(&self.url[..], req.to_string())
+    }
+}
+
+struct Getter {
+    /// A representation of the output of the WebSocket connection.
+    output: Sender,
+    /// The json request data which is formatted string type.
+    request: String,
+    /// The sending side of a channel.
+    result: ThreadOut<String>,
+}
+
+impl Handler for Getter {
+    /// Called when the WebSocket handshake is successful and the connection is open for sending
+    /// and receiving messages.
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        self.output.send(self.request.clone())
+            .map_err(|_| Error::new(ErrorKind::Internal, "must connect"))?;
+
+        Ok(())
+    }
+
+    /// Called on incoming messages.
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let txt = msg.as_text()?;
+        let value: serde_json::Value = serde_json::from_str(txt)
+            .map_err(|_| Error::new(ErrorKind::Internal, "Request deserialization is infallible; qed"))?;
+
+        let hex_str = match value["result"].as_str() {
+            Some(res) => res.to_string(),
+            None => "0x00".to_string(), // TODO:
+        };
+
+        self.result.send(hex_str)
+            .map_err(|_| Error::new(ErrorKind::Internal, "must run"))?;
+        self.output.close(CloseCode::Normal)?;
+        Ok(())
+    }
+}
+
+// impl Getter {
+//     pub fn new(output: Sender, request: String, result: ThreadOut<String>) -> Self {
+//         Getter {
+//             output,
+//             request,
+//             result,
+//         }
+//     }
+
+//     pub fn get_response(&self, url: &str, req: String) -> Result<String> {
+//         let (tx, rx) = unbounded();
+
+//         crossbeam::scope(|scope| {
+//             scope.spawn(move |_| {
+//                 connect(url.to_owned(), |output| {
+//                     // self::new(output, req, tx)
+//                     Getter {
+//                         output,
+//                         request: req.clone(),
+//                         result: tx.clone(),
+//                     }
+//                 }).expect("must connect")
+//             });
+//         }).expect("must run");
+
+//         Ok(rx.recv().expect("must not be empty"))
+//     }
+// }
+
+struct Submitter {
+    output: Sender,
+    request: String,
+    result: ThreadOut<Hash>,
+}
+
+impl Handler for Submitter {
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        self.output.send(self.request.clone())
+            .map_err(|_| Error::new(ErrorKind::Internal, "must connect"))?;
+
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let txt = msg.as_text()?;
+        let value: serde_json::Value = serde_json::from_str(txt)
+            .map_err(|_| Error::new(ErrorKind::Internal, "Request deserialization is infallible; qed"))?;
+
+        match value["id"].as_str() {
+            Some(id) => {
+                match id.parse::<u32>() {
+                    Ok(REQUEST_SUBMIT) => {
+                        match value.get("error") {
+                            Some(err) => {},
+                            None => {},
+                        }
+                    },
+                    Ok(_) => {
+
+                    },
+                    Err(_) => {
+
+                    }
+                }
+            },
+            None => {
+                match value["method"].as_str() {
+                    Some("author_extrinsicUpdate") => {
+                        match value["params"]["result"].as_str() {
+                            Some(res) => {},
+                            None => {
+                                self.result.send(hexstr_to_hash(value["params"]["result"]["finalized"].as_str().unwrap().to_string())).unwrap();
+                                self.output.close(CloseCode::Normal).unwrap();
+                            },
+                        }
+                    },
+                    Some(_) => {},
+                    None => {},
+                }
+            }
+        };
+
+        Ok(())
     }
 }
 
@@ -81,42 +202,23 @@ pub fn get_response(url: &str, req: String) -> Result<String> {
     Ok(rx.recv().expect("must not be empty"))
 }
 
-struct Getter {
-    /// A representation of the output of the WebSocket connection.
-    output: Sender,
-    /// The json request data which is formatted string type.
-    request: String,
-    /// The sending side of a channel.
-    result: ThreadOut<String>,
-}
+// TODO: Getting abstract
+pub fn submit(url: &str, req: String) -> Result<Hash> {
+    let (tx, rx) = unbounded();
 
-impl Handler for Getter {
-    // Start handshake from clients
-    fn on_open(&mut self, _: Handshake) -> Result<()> {
-        self.output.send(self.request.clone())
-            .map_err(|_| Error::new(ErrorKind::Internal, "must connect"))?;
-        Ok(())
-    }
+    crossbeam::scope(|scope| {
+        scope.spawn(move |_| {
+            connect(url.to_owned(), |output| {
+                Submitter {
+                    output,
+                    request: req.clone(),
+                    result: tx.clone(),
+                }
+            }).expect("must connect")
+        });
+    }).expect("must run");
 
-    fn on_message(&mut self, msg: Message) -> Result<()> {
-        let txt = msg.as_text()?;
-        let value: serde_json::Value = serde_json::from_str(txt)
-            .map_err(|_| Error::new(ErrorKind::Internal, "Request deserialization is infallible; qed"))?;
-
-        let hex_str = match value["result"].as_str() {
-            Some(res) => res.to_string(),
-            None => "0x00".to_string(),
-        };
-
-        self.result.send(hex_str)
-            .map_err(|_| Error::new(ErrorKind::Internal, "must run"))?;
-        self.output.close(CloseCode::Normal)?;
-        Ok(())
-    }
-}
-
-struct Subscriber {
-
+    Ok(rx.recv().expect("must not be empty"))
 }
 
 #[cfg(test)]
@@ -129,5 +231,10 @@ mod tests{
         let res_str = api.get_storage("Balances", "transactionBaseFee", None).unwrap();
         let res = hexstr_to_u256(res_str);
         println!("TransactionBaseFee is {}", res);
+    }
+
+    #[test]
+    fn test_submit_extrinsic() {
+        let api = Api::init(Url::Local).unwrap();
     }
 }
